57fb084ec98d2cd356cfce5ae93292e24b569565
huiping_guo <huiping.guo@dena.jp>
2018-08-20 19:11:23 +0900

    project init

---
+++ HPLiveKit/HPLiveKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HPLiveKit
+//
+//  Created by Huiping Guo on 2018/08/20.
+//  Copyright © 2018 Huiping Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
