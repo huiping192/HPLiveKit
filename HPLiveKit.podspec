d982a157c69060ce40387fb7726f420f68381f17
huiping_guo <huiping.guo@dena.jp>
2022-08-25 23:16:18 +0900

    test

--- HPLiveKit.podspec
+++ HPLiveKit.podspec
@@ -27,7 +27,7 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = '8.0'
   s.swift_version = '4.0'
-  s.source_files = 'HPLiveKit/Classes/**/*.{swift,h,m}'
+  s.source_files = 'Sources/HPLiveKit/**/*.{swift,h,m}'
   
   s.frameworks = "VideoToolbox", "AudioToolbox","AVFoundation","Foundation","UIKit"
   s.dependency 'HPLibRTMP' , '~> 0.0.3'
--- Package.swift
+++ Package.swift
@@ -5,16 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "HPLiveKit",
+    platforms: [
+            .iOS(.v8)
+        ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/huiping192/HPLibRTMP", from: "0.0.3"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "HPLiveKit",
-            dependencies: []),
+            dependencies: [
+                "HPLibRTMP"
+            ],
+            linkerSettings: [
+                .linkedFramework("VideoToolbox"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("UIKit"),
+            ]),
         .testTarget(
             name: "HPLiveKitTests",
             dependencies: ["HPLiveKit"]),
