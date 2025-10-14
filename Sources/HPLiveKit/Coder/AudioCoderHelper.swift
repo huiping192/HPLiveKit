9fa66938d6671b4527d5f18730b464cf17370c10
Huiping Guo <huiping192@gmail.com>
2025-10-15 08:26:22 +0900

    Remove debug logging from AudioCoderHelper

--- Sources/HPLiveKit/Coder/AudioCoderHelper.swift
+++ Sources/HPLiveKit/Coder/AudioCoderHelper.swift
@@ -15,36 +15,11 @@ extension CMSampleBuffer {
     var audioBufferList = AudioBufferList()
     var data = Data()
     var blockBuffer: CMBlockBuffer?
-
+    
     CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(self, bufferListSizeNeededOut: nil, bufferListOut: &audioBufferList, bufferListSize: MemoryLayout<AudioBufferList>.size, blockBufferAllocator: nil, blockBufferMemoryAllocator: nil, flags: 0, blockBufferOut: &blockBuffer)
-
+    
     let buffers = UnsafeBufferPointer<AudioBuffer>(start: &audioBufferList.mBuffers, count: Int(audioBufferList.mNumberBuffers))
-
-    #if DEBUG
-    // Diagnostic logging for audio format
-    if let formatDesc = CMSampleBufferGetFormatDescription(self),
-       let asbd = CMAudioFormatDescriptionGetStreamBasicDescription(formatDesc)?.pointee {
-      print("[AudioCoderHelper] Audio Buffer Info:")
-      print("  Number of buffers: \(audioBufferList.mNumberBuffers)")
-      print("  Format ID: 0x\(String(format: "%X", asbd.mFormatID))")
-      print("  Format Flags: 0x\(String(format: "%X", asbd.mFormatFlags))")
-
-      // Check if non-interleaved
-      let isNonInterleaved = (asbd.mFormatFlags & kAudioFormatFlagIsNonInterleaved) != 0
-      print("  Is Non-Interleaved (planar): \(isNonInterleaved)")
-      print("  Channels per frame: \(asbd.mChannelsPerFrame)")
-
-      for (index, audioBuffer) in buffers.enumerated() {
-        print("  Buffer[\(index)]: channels=\(audioBuffer.mNumberChannels), size=\(audioBuffer.mDataByteSize) bytes")
-      }
-
-      if audioBufferList.mNumberBuffers > 1 {
-        print("  ⚠️  WARNING: Multiple buffers detected! This might be non-interleaved audio.")
-        print("  Current code will concatenate buffers sequentially, which is WRONG for interleaved encoding!")
-      }
-    }
-    #endif
-
+    
     for audioBuffer in buffers {
       let frame = audioBuffer.mData?.assumingMemoryBound(to: UInt8.self)
       data.append(frame!, count: Int(audioBuffer.mDataByteSize))
