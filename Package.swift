a7e6e54dfee57767f136c8a1531f304a2af71dbd
Huiping Guo <huiping192@gmail.com>
2025-10-20 21:25:33 +0900

    revert HPRTMP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         name: "HPLiveKit",
         targets: ["HPLiveKit"]),
     ], dependencies: [
-      .package(path: "../HPRTMP")
+      .package(url: "https://github.com/huiping192/HPRTMP.git", revision: "85e51c444d850f074b64a63dbfa75d69ebd21c21")
     ],
     targets: [
       .target(
